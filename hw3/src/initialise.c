6a7a5f458878775c1c1e186bb2fea862a392a575
Vijayabharathi Murugan <vmurugan@cs.stonybrook.edu>
2024-10-24 20:40:06 -0400

    feat(hw3): update initialisation functions

--- hw3/src/initialise.c
+++ hw3/src/initialise.c
@@ -1,53 +1,45 @@
-#include "debug.h"
-#include "sfmm.h"
-#include "initialise.h"
+#include "sfmm_utils.h"
 
 /*
- * Helper function to intialise prologue
+ * Helper function to intialize prologue
  */
 void update_prologue() {
-    // TODO: make this function better
-    info("updating prologue");
-    long int *heap_start = (long int *) sf_mem_start();
-    // Prologue header
-    *(heap_start + offset / 8) = 0x20 + 0x10;
+    info("Updating prologue...");
+    sf_block *pp = (sf_block *)((char *) sf_mem_start() + offset);
+    // Prologue header & footer
+    pp->header = 0x20 + 0b10000;
+    *get_footer(pp) = pp->header;
+    debug("prologue: %p", pp);
 }
 
 /*
- * Helper function to initialise epilogue
+ * Helper function to initialize epilogue
  */
 void update_epilogue() {
-    // TODO: make this function better
-    info("updating epilogue");
-    long int *heap_end = (long int *) sf_mem_end();
-
-    // Prologue header
-    *(heap_end - 1) = 0x0 + 0b10000;
+    info("Updating epilogue...");
+    sf_block *ep = (sf_block *)((char *) sf_mem_end() - MEMROWSIZE);
+    // Epilogue header & footer
+    ep->header = 0x0 + 0b10000;
+    debug("epilogue: %p", ep);
 }
 
 /*
  * Helper function to initalise free block
  */
-void first_free_block() {
+void add_wilderness_block() {
     // TODO: make this function better
-    info("updating first free block");
-    long int *block_start = ((long int *) sf_mem_start()) + offset / 8 + 4;
-    long int *block_end = ((long int *) sf_mem_end()) - 1;
-    size_t block_size = (size_t) block_end - (size_t) block_start;
-    debug("%p", block_start);
-    debug("%p", block_end);
+    info("Adding Wilderness block...");
+    sf_block *bp = (sf_block *)((char *) sf_mem_start() + offset + 4 * MEMROWSIZE);
+    size_t block_size = (size_t) sf_mem_end() - (size_t) bp - MEMROWSIZE;
 
-    debug("%ld", block_size);
-    debug("%ld", block_size % 32);
-    
-    // Header * footer
-    *block_start = block_size + 0b1000;
-    *(block_end - 1) = block_size + 0b1000;
-    
+    bp->header = block_size + 0b1000;
+    *get_footer(bp) = bp->header;
+    debug("wilderness block header %ld %ld", bp->header, block_size);
+
     // pointers
-    sf_free_list_heads[8].body.links.next = (struct sf_block *) block_start;
-    sf_free_list_heads[8].body.links.prev = (struct sf_block *) block_start;
-
-    *(block_start + 1) = (long int) &sf_free_list_heads[8];
-    *(block_start + 2) = (long int) &sf_free_list_heads[8];
+    bp->body.links.next = &sf_free_list_heads[8];
+    bp->body.links.prev = &sf_free_list_heads[8];
+    
+    sf_free_list_heads[8].body.links.next = bp;
+    sf_free_list_heads[8].body.links.prev = bp;
 }
