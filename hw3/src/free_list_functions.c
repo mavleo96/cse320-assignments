12254b9fa8de65e42cd61872981dbb9cc9cd9bf0
Vijayabharathi Murugan <vmurugan@cs.stonybrook.edu>
2024-10-25 19:00:59 -0400

    doc(hw3): update docstring and comments

--- hw3/src/free_list_functions.c
+++ hw3/src/free_list_functions.c
@@ -1,5 +1,8 @@
 #include "sfmm_utils.h"
 
+/*
+ * Helper function to get free list index for given size
+ */
 int get_free_list_index_for_size(size_t size) {
     // Check if size is aligned; return -1 if not
     if (size % ALIGNMENT != 0) return -1;
@@ -18,43 +21,74 @@ int get_free_list_index_for_size(size_t size) {
     else                return -1;  // TODO: this means size zero; check if -1 needs to be returned or NULL handling needed
 }
 
+/*
+ * Helper function to find a block of given size in free list of given index
+ */
 sf_block *find_in_free_list_i(int index, size_t size) {
+    // Sentinel node of free list
     sf_block *list_head = &sf_free_list_heads[index];
+
+    // Traverse through free list
     sf_block *bp = &sf_free_list_heads[index];
-    
+    // Continue checking for free block until next block is sentinel node
     while (bp->body.links.next != list_head) {
         bp = bp->body.links.next;
         if (BLOCKSIZE(bp) >= size) {
             return bp;
         }
     }
+    // Return NULL if no free block found
     return NULL;
 }
 
-
+/*
+ * Helper function to remove a block from free list
+ */
 void remove_block_from_free_list(sf_block *bp) {
-    size_t block_size = BLOCKSIZE(bp);
+    // Validate block pointer 
+    if (bp == NULL) {
+        error("NULL pointer passed!");
+        return;
+    }
+    if ((bp->body.links.next == NULL) || (bp->body.links.prev == NULL)) {
+        error("Pointer with NULL links passed!");
+        return;
+    }
+    // TODO: clean up below code
+    size_t blocksize = BLOCKSIZE(bp);
     // Allocated bit change
     bp->header |= 0b10000;
 
     // Prev allocated bit change
     // TODO: what if this is a free block?
-    *(sf_header *)((char *) bp + block_size) |= 0b1000;
+    *(sf_header *)((char *) bp + blocksize) |= 0b1000;
 
     // TODO: next and prev should not be NULL here
+    // Update the links in next and prev block
     bp->body.links.next->body.links.prev = bp->body.links.prev;
     bp->body.links.prev->body.links.next = bp->body.links.next;
     return;
 }
 
+/*
+ * Helper function to add a block to appropriate free list
+ */
 void add_block_to_free_list(sf_block *bp, int wilderness_signal) {
-    size_t block_size = BLOCKSIZE(bp);
-    int index = wilderness_signal ? NUM_FREE_LISTS - 1 : get_free_list_index_for_size(block_size);
-    sf_block *list_head = &sf_free_list_heads[index];
+    // Validate block pointer 
+    if (bp == NULL) {
+        error("NULL pointer passed!");
+        return;
+    }
+    size_t blocksize = BLOCKSIZE(bp);
+    // TODO: internalize the wilderness_signal arg
+    int index = wilderness_signal ? NUM_FREE_LISTS - 1 : get_free_list_index_for_size(blocksize);
 
+    // Add the free block to the list by updating links
+    sf_block *list_head = &sf_free_list_heads[index];
     bp->body.links.prev = list_head;
     bp->body.links.next = list_head->body.links.next;
 
+    // Update the links in next and prev block
     bp->body.links.next->body.links.prev = bp;
     bp->body.links.prev->body.links.next = bp;
 
--- hw3/src/sfmm.c
+++ hw3/src/sfmm.c
@@ -11,33 +11,45 @@
 
 void *sf_malloc(size_t size) {
     // Initialize the sf_malloc
-    if (global_ != 0x1) sf_init();
+    if (init_flag != 0x1) sf_init();
 
-    // Check if requested size is 0; return NUll if true
+    info("Allocating memory...");
+    // Check if requested size is 0; return NULL if true
     if (size == 0) {
-        info("Request size is 0");
+        debug("sf_malloc: request size is 0; returning NULL");
         return NULL;
+    } else {
+        debug("sf_malloc: request size is %ld", size);
     }
 
-    // Convert the payload size to min required size and get 
-    size_t required_size = MIN_REQUIRED_BLOCKSIZE(size);
-    int index = get_free_list_index_for_size(required_size);
+    // Convert the payload size to min required size
+    size_t rsize = MIN_REQUIRED_BLOCKSIZE(size);
 
-    // Find block of required_size
+    // Find block of required size
+    info("Searching free block of size %ld...", rsize);
     sf_block *bp;
+    int index = get_free_list_index_for_size(rsize);
+    // For loop to search in jth free lists for j from index to max
     for (int j = index; j < NUM_FREE_LISTS; j++) {
-        bp = find_in_free_list_i(j, required_size);
+        // Search for free block in jth free list
+        bp = find_in_free_list_i(j, rsize);
+
         if (bp == NULL) {
+            // If free block not found in jth free list then continue
             if (j == NUM_FREE_LISTS - 1) {
+                // If free block not found in last free list then expand heap
+                info("No free blocks found...");
                 sf_block *fbp = expand_heap();
                 fbp = coalesce_block(fbp);
                 add_block_to_free_list(fbp, 1);
                 j--;
             } else continue;
         } else {
+            info("Free block of size %ld found...", BLOCKSIZE(bp));
             remove_block_from_free_list(bp);
-            if (BLOCKSIZE(bp) - required_size >= ALIGNMENT) {
-                sf_block *rbp = break_block(bp, required_size);
+            if (BLOCKSIZE(bp) - rsize >= ALIGNMENT) {
+                // If free block found can be broken without splinter
+                sf_block *rbp = break_block(bp, rsize);
                 add_block_to_free_list(rbp, (j == NUM_FREE_LISTS - 1) ? 1 : 0);
             }
         }
@@ -47,18 +59,22 @@ void *sf_malloc(size_t size) {
 }
 
 void sf_free(void *pp) {
+    // Validate the pointer to be freed
     info("Validating the pointer to be freed...");
     validate_pointer(pp, 0);
 
+    // Free the block by updating header & footer
     info("Freeing the memory allocated at %p", pp);
     sf_block *bp = (sf_block *)((char *) pp - MEMROWSIZE);
     bp->header &= ~ 0b10000;
     *FOOTER_POINTER(bp) = bp->header;
 
+    // Update prev_allocated_bit in next block
     sf_block *nbp = NEXT_BLOCK_POINTER(bp);
     nbp->header &= ~ 0b1000;
     if (!ALLOCATED_BIT(nbp)) *FOOTER_POINTER(nbp) = nbp->header;
 
+    // Coalesce the block and add to free list
     bp = coalesce_block(bp);
     add_block_to_free_list(bp, 0);
 
@@ -66,25 +82,31 @@ void sf_free(void *pp) {
 }
 
 void *sf_realloc(void *pp, size_t rsize) {
+    // Validate the pointer to be reallocated
     info("Validating the pointer to be reallocated...");
+    // TODO: update mode arg below
     validate_pointer(pp, 0);
 
-    info("Freeing the memory allocated at %p", pp);
+    // Calculate block pointer and block sizes
     sf_block *bp = (sf_block *)((char *) pp - MEMROWSIZE);
     size_t block_size = BLOCKSIZE(bp);
     size_t new_block_size = MIN_REQUIRED_BLOCKSIZE(rsize);
 
     if (rsize == 0) {
+        // If block is to be reallocated to size 0 then call sf_free and return null
         sf_free(pp);
         return NULL;
     } else if (new_block_size == block_size) {
+        // If block is to be reallocated to same size then return pp
         return pp;
     } else if (new_block_size > block_size) {
+        // If block is to be reallocated to larger size then call sf_malloc, copy contents and call sf_free
         void *npp = sf_malloc(rsize);
         memcpy(npp, pp, block_size - MEMROWSIZE);
         sf_free(pp);
         return npp;
     } else {
+        // If block is to be reallocated to smaller size then break block, coalesce remainder contents and add to free list
         sf_block *rbp = break_block(bp, new_block_size);
         rbp = coalesce_block(rbp);
         add_block_to_free_list(rbp, 0);
