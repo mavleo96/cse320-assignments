31a00a68fe2d679b40ad009f65196e1a958a1348
Vijayabharathi Murugan <vmurugan@cs.stonybrook.edu>
2024-11-22 23:50:33 -0500

    feat(hw4): update auxillary functions

--- hw4/src/aux.c
+++ hw4/src/aux.c
@@ -9,8 +9,6 @@ void validargs(int argc, char *argv[], char *argrecipe[], char *argfile[], int *
     int i = 1;
 
     // Initialize variables for default values
-    // TODO: Veridy that eggs_benedict need not be dynamically assigned
-    *argrecipe = "eggs_benedict";
     *argfile = "rsrc/cookbook.ckb";
     *argmaxcooks = 1;  // Default to 1 cook if not specified
 
@@ -20,8 +18,8 @@ void validargs(int argc, char *argv[], char *argrecipe[], char *argfile[], int *
             // TODO: Check if you need to exit with failure for multiple -c
             if (i + 1 < argc) {
                 // TODO: Fail for c <0 or invalid case
-                *argmaxcooks = atoi(argv[i + 1]);
                 i++;
+                *argmaxcooks = atoi(argv[i]);
             } else {
                 error("missing value for '-c' option");
                 exit(EXIT_FAILURE);
@@ -32,8 +30,8 @@ void validargs(int argc, char *argv[], char *argrecipe[], char *argfile[], int *
             // TODO: Check if you need to exit with failure for multiple -f
             if (i + 1 < argc) {
                 // TODO: Fail for empty string
-                *argfile = argv[i + 1];
                 i++;
+                *argfile = argv[i];
             } else {
                 error("missing filename for '-f' option");
                 exit(EXIT_FAILURE);
@@ -41,12 +39,13 @@ void validargs(int argc, char *argv[], char *argrecipe[], char *argfile[], int *
         } 
         // TODO: Below code if you can't defaualt main recipe with static assignment
         // Assign recipe if not already assigned
-        // else if (*argrecipe == NULL) {
+        else if (*argrecipe == NULL) {
+            *argrecipe = argv[i];
+            return;
+        }
+        // else if (argc == i + 1) {
         //     *argrecipe = argv[i];
         // }
-        else if (argc == i + 1) {
-            *argrecipe = argv[i];
-        }
         // If no valid argument is found
         else {
             error("invalid argument: %s", argv[i + 1]);
@@ -55,4 +54,32 @@ void validargs(int argc, char *argv[], char *argrecipe[], char *argfile[], int *
         // Increment i
         i++;
     }
+}
+
+/*
+ * Function to get recipe pointer for a given name from cookbook at given pointer
+ */
+RECIPE *get_main_recipe(COOKBOOK *cbp, char *name) {
+    if (!name) {
+        name = cbp->recipes->name;
+        return cbp->recipes;
+    }
+
+    for(RECIPE *rp = cbp->recipes; rp != NULL; rp = rp->next) {
+        if(!strcmp(rp->name, name))
+            return rp;
+    }
+    return NULL;
+}
+
+/*
+ * Function to length of recipe link recipe at given pointer
+ */
+int recipe_link_length(RECIPE_LINK *rlp) {
+    int len = 0;
+    while (rlp) {
+        len++;
+        rlp = rlp->next;
+    }
+    return len;
 }
